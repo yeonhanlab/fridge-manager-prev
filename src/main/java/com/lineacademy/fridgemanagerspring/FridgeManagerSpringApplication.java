c9cef166f9520ac530cabe5e74e4c3696f27d443
yeonhan <yeonhanlab@gmail.com>
2026-08-12 14:23:16 +0900

    초기화

---
+++ src/main/java/com/lineacademy/fridgemanagerspring/FridgeManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lineacademy.fridgemanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeManagerSpringApplication.class, args);
+    }
+
+}
